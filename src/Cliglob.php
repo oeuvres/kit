cccbdf1a21fe19bbbb4b96486259642a292116d3
Frédéric Glorieux <frederic.glorieux@fictif.org>
2024-02-19 00:17:43 +0100

    Static class usable in phpunit test

--- src/Cliglob.php
+++ src/Cliglob.php
@@ -13,25 +13,42 @@ use Psr\Log\LogLevel;
 use Oeuvres\Kit\{Filesys, Log};
 use Oeuvres\Kit\Logger\{LoggerCli};
 
-abstract class Cliglob
+class Cliglob
 {
     /** Options */
-    public static $options;
-    /** The source format name */
-    const SRC_FORMAT = self::SRC_FORMAT;
-    /** The prefered source format name */
-    const SRC_EXT = self::SRC_EXT;
-    /** The destination format name */
-    const DST_FORMAT = self::DST_FORMAT;
-    /** A destination extension for generated files */
-    const DST_EXT = self::DST_EXT;
-    /** An optional destination prefix */
-    const DST_PREFIX = "";
+    private static $options = [];
+
+    /**
+     * Get an option
+     */
+    public static function get(string $name, $default = null)
+    {
+        if (!isset(self::$options[$name])) {
+            return $default;
+        }
+        return self::$options[$name];
+    }
+
+    /**
+     * Set an option
+     */
+    public static function put(string $name, $value):void
+    {
+        self::$options[$name] = $value;
+    }
+
+    /**
+     * Set multiple options
+     */
+    public static function putAll(array $options):void
+    {
+        self::$options = array_merge(self::$options, $options);
+    }
 
     /**
      * Parse command line arguments and process files
      */
-    public static function glob($action)
+    public static function glob(callable $action)
     {
         global $argv;
         $shortopts = "";
@@ -41,12 +58,12 @@ abstract class Cliglob
         $shortopts .= "d:"; // output directory
         $shortopts .= "t:"; // template file
         $rest_index = null;
-        static::$options = getopt($shortopts, [], $rest_index);
+        self::putAll(getopt($shortopts, [], $rest_index));
         $pos_args = array_slice($argv, $rest_index);
         if (count($pos_args) < 1) {
-            exit(static::help());
+            exit(self::help());
         }
-        if (isset(static::$options['v'])) {
+        if (isset(self::$options['v'])) {
             Log::setLogger(new LoggerCli(LogLevel::DEBUG));
         }
         else {
@@ -63,9 +80,9 @@ abstract class Cliglob
                 if (!Filesys::readable($src_file)) {
                     continue;
                 }
-                $dst_file = static::destination($src_file);
+                $dst_file = self::destination($src_file);
                 // test freshness
-                if (isset(static::$options['f'])); // force
+                if (isset((self::$options['f']))); // force
                 else if (!file_exists($dst_file)); // destination not exists
                 else if (filemtime($src_file) < filemtime($dst_file)) continue;
                 $action($src_file, $dst_file);
@@ -95,8 +112,8 @@ abstract class Cliglob
     {
         list($called) = get_included_files();
         $help = "
-Tranform ".static::SRC_FORMAT." files in ".static::DST_FORMAT."
-    php ".basename($called)." (options)* \"src_dir/*".static::SRC_EXT."\"
+Tranform " . self::get('src_format')." files in ". self::get('dst_format') ."
+    php ".basename($called)." (options)* \"src_dir/*." . self::get('src_ext') . "\"
 
 PARAMETERS
 globs           : + files or globs
@@ -105,7 +122,7 @@ OPTIONS
 -h              : ? print this help
 -f              : ? force deletion of destination file (no test of freshness)
 -d dst_dir      : ? destination directory for generated files
--t template".static::DST_EXT." : * template files
+-t template." . self::get('dst_ext') . " : * template files
 -v              : ? verbose mode
 ";
         return $help;
@@ -116,13 +133,9 @@ OPTIONS
      */
     static public function destination($src_file): string
     {
-        if (!isset(static::$options['d'])) {
-            $dst_dir = dirname($src_file) . DIRECTORY_SEPARATOR;
-        } else {
-            $dst_dir = Filesys::normdir(static::$options['d']);
-        }
+        $dst_dir = Filesys::normdir(self::get('d', dirname($src_file) . DIRECTORY_SEPARATOR));
         $dst_name =  pathinfo($src_file, PATHINFO_FILENAME);
-        $dst_file = $dst_dir . static::DST_PREFIX . $dst_name . static::DST_EXT;
+        $dst_file = $dst_dir . self::get('dst_prefix', '') . $dst_name . '.' . self::get('dst_ext');
         return $dst_file;
 
     }
